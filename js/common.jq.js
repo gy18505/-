2b4e6b00737f2c8601ff1b8680bd99f28dfb4718
gy18505 <gy18505@163.com>
2017-12-06 18:05:36 +0800

    daily update
    a temp first version

--- js/common.jq.js
+++ js/common.jq.js
@@ -1,13 +1,24 @@
-$(function(){
+$(function () {
 
   // navbar on mobile hover
-  $('.navbar-mobile .menu-list>li').has('.sublist').hover(function () {
-    $(this).css('height', '200px');
+  $('.navbar-mobile .menu-list>li').click(function () {
+    var $this = $(this);
+    $this.hasClass('active') ? $this.removeClass('active') : $this.addClass('active').siblings().removeClass('active');
+  }).has('.sublist').hover(function () {
+    var $subli = $('.sublist', this).children();
+    $(this).css('height', ($subli.length + 1) * $subli.eq(0).css('height').slice(0, -2));
   }, function () {
     $(this).removeAttr('style');
   }).click(function (e) {
-    e ? e.preventDefault() : window.event.returnValue = false;
-    $(this).css('height', '200px').siblings().removeAttr('style');
+    if ($(e.target).parents('.sublist').length) {
+      return;
+    }
+    var $subli = $('.sublist', this).children();
+    if ($(this).hasClass('active')) {
+      $(this).css('height', ($subli.length + 1) * $subli.eq(0).css('height').slice(0, -2)).siblings().removeAttr('style');
+    } else {
+      $(this).css('height', $subli.eq(0).css('height'));
+    }
   });
 
   // menu-list hover
@@ -16,4 +27,5 @@ $(function(){
   }, function () {
     $(this).find('.sublist').hide();
   });
+
 })
