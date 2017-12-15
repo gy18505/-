2f1dad3dc20f9f0951593c2fb980404409778777
gy18505 <gy18505@163.com>
2017-12-15 13:43:56 +0800

    bugs fixed

--- js/index.js
+++ js/index.js
@@ -6,26 +6,6 @@ $(function () {
   // vision init
   new Rellax('.rellax');
 
-  // service-logo hover
-  $('.service-logo').hover(function () {
-    var $logo = $(this).children('img').eq(0).css({
-      'transform': 'rotate(45deg)',
-      '-webkit-transform': 'rotate(45deg)',
-      '-moz-transform': 'rotate(45deg)',
-      '-ms-transform': 'rotate(45deg)',
-      '-o-transform': 'rotate(45deg)'
-    });
-    _timer = setTimeout(function () {
-      $logo.attr('src', 'images/' + $logo.data('logo') + '-transparent.png');
-    }, 100);
-  }, function () {
-    clearTimeout(_timer);
-    var $logo = $(this).children('img').eq(0).removeAttr('style');
-    _timer = setTimeout(function () {
-      $logo.attr('src', 'images/' + $logo.data('logo') + '.png');
-    }, 100);
-  });
-  
   var mySwiper = new Swiper ('.banner', {
     autoplay : 5000,
     direction: 'horizontal',
